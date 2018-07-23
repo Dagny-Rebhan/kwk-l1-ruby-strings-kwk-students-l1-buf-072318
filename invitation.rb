662d7ecf0724ad262372930d4c4d8f765482faf7
Dagny-Rebhan <drebhan@buffaloseminary.org>
2018-07-23 20:22:25 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -9,7 +9,11 @@ guest_name = gets.chomp
 puts "What is the name of the party?"
 party_name = gets.chomp
 
-puts "What is the date of the party?"
+puts "What is the date (not time) of the party?"
 date = gets.chomp
 
-puts ""
+puts "What time is the party?"
+time = gets.chomp
+
+puts "Who is hosting the party?"
+host_name = gets.chomp
